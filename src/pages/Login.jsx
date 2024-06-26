b67907839a17f67d454363a91810f8bf9948f794
Pablo Zavala <juanparamosae@gmail.com>
2024-06-26 12:41:59 -0600

    Configutacion de Redux

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import { useSelector } from 'react-redux';
 
 export const Login = () => {
+  const { status } = useSelector(state => state.auth);
+
   return (
-    <div>Login</div>
+    <div>Login {status} </div>
   )
 }
---
+++ src/store/authSlice.js
@@ -0,0 +1,28 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const authSlice = createSlice({
+  name: 'auth',
+  initialState: {
+    status: 'checking',
+    errorMessage: undefined,
+  },
+  reducers: {
+    onChecking: (state) => {
+      state.status = 'checking';
+      state.errorMessage = undefined;
+    },
+    onLogin: (state, { payload }) => {
+      state.status = 'authenticated';
+      state.errorMessage = undefined;
+    },
+    onLogout: (state, { payload }) => {
+      state.status = 'not-authenticated';
+      state.errorMessage = payload;
+    },
+    clearErrorMessage: (state) => {
+      state.errorMessage = undefined;
+    }
+  }
+});
+
+export const { onChecking, onLogin, onLogout, clearErrorMessage } = authSlice.actions;
